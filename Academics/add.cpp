39330ea81c9aa6c405247326ce3d10f1e3a6d3c8
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2024-04-18 22:21:53 +0530

    Add files via upload

---
+++ Academics/add.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float a, b;
+  cout << "Enter two number: ";
+  cin >> a >> b;
+  cout << "Your number are " << a << " " << b << endl;
+  cout << a << "+" << b << "=" << a + b << endl;
+  cout << a << "-" << b << "=" << a - b << endl;
+  cout << a << "*" << b << "=" << a * b << endl;
+  cout << a << "/" << b << "=" << a / b << endl;
+}
